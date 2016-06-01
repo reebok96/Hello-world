64aaffaf4143fb774ef984d5199c0386b8d0d36e
suloeva <maria-exclusive@mail.ru>
2016-06-01 16:37:44 +0500

    Add files via upload

---
+++ Lab.java
@@ -0,0 +1,22 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: student
+ * Date: 22.09.15
+ * Time: 15:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Lab {
+    public static void main(String args[]){
+        Laba a=new Laba();
+        a.Array();
+        a.InitArray();
+        a.minmax();
+        a.copymas();
+        a.Sort();
+        a.summ();
+        a.kol();
+        a.otr();
+        a.avg();
+        a.otrezok();
+    }
+}
